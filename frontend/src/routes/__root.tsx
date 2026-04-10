b68c8c7ed2441b364f247b2ca703721ca3dbce93
Tim Lin <81815607+TimmyLin21@users.noreply.github.com>
2026-04-10 14:17:40 +1000

    Init frontend setup

---
+++ frontend/src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+
+export const Route = createRootRoute({
+    component: RootComponent,
+    notFoundComponent: () => {
+        return (
+            <div>
+                <p>This is the notFoundComponent configured on root route</p>
+                <Link to="/">Start Over</Link>
+            </div>
+        )
+    },
+})
+
+function RootComponent() {
+    return (
+        <>
+            <div className="p-2 flex gap-2 text-lg border-b">
+                <Link
+                    to="/"
+                    activeProps={{
+                        className: 'font-bold',
+                    }}
+                    activeOptions={{ exact: true }}
+                >
+                    Home
+                </Link>{' '}
+            </div>
+            <hr />
+            <Outlet />
+        </>
+    )
+}
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+    component: Home,
+})
+
+function Home() {
+
+    return (
+        <>
+            <div>
+                Hello World
+            </div>
+        </>
+    )
+}
